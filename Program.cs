a41d5a4e890e8dedd6dc2f5be7d2c25cc9319659
unknown <valexandre@br.fujitsu.com>
2018-06-20 17:46:52 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exemplo_kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
